304dcf014c969a378ab2f9c22db10179b10d5793
yansq <yan_sq@yeah.net>
2023-05-05 16:09:58 +0800

    UnoCSS config

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 class="red">Vite + React</h1>
+      <h1 text-red>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
